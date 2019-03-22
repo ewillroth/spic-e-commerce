d1ab7714e37b3d25fe3c7cf743dc81c249ccf679
Eric Willroth <ewillroth@gmail.com>
2019-03-22 14:22:05 -0500

    point to build file

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const ctrl = require('./controller')
 const app = express()
 const port = process.env.PORT || 4444
-
+app.use(express.static(`${__dirname}/../build`));
 app.use(json())
 app.use(cors())
 app.use(session({
