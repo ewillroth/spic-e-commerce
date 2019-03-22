4f3a04e0512c4d030ee4f02c940129423be88c99
Eric Willroth <ewillroth@gmail.com>
2019-03-22 09:30:13 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import routes from './routes';
 
 class App extends Component {
 	render() {
 		return (
-		<div>
-
-		</div>
+		<>
+			{routes}
+		</>
 		);
 	}
 }
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Splash from './Splash';
+
+export default (
+<Switch>
+	<Route path="/"><Splash/></Route>
+</Switch>
+)
